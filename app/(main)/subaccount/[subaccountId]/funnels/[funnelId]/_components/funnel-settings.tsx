4d9184dc11effea3570caf919455d8034154d441
Adam <56221591+AdamK73@users.noreply.github.com>
2024-03-06 01:34:09 +0100

    removing errors

--- app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/_components/funnel-settings.tsx
+++ app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/_components/funnel-settings.tsx
@@ -1,42 +1,41 @@
-import React from 'react'
+import React from "react";
 
-import { Funnel, SubAccount } from '@prisma/client'
-import { db } from '@/lib/db'
-import { getConnectAccountProducts } from '@/lib/stripe/stripe-actions'
+import { Funnel, SubAccount } from "@prisma/client";
+import { db } from "@/lib/db";
+import { getConnectAccountProducts } from "@/lib/stripe/stripe-actions";
 
-
-import FunnelForm from '@/components/forms/funnel-form'
+import FunnelForm from "@/components/forms/funnel-form";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
-import FunnelProductsTable from './funnel-products-table'
+} from "@/components/ui/card";
+import FunnelProductsTable from "./funnel-products-table";
 
 interface FunnelSettingsProps {
-  subaccountId: string
-  defaultData: Funnel
+  subaccountId: string;
+  defaultData: Funnel;
 }
 
 const FunnelSettings: React.FC<FunnelSettingsProps> = async ({
   subaccountId,
   defaultData,
 }) => {
-  //CHALLENGE: go connect your stripe to sell products
+  //CHALLENGE: go  connect your stripe to sell products
 
   const subaccountDetails = await db.subAccount.findUnique({
     where: {
       id: subaccountId,
     },
-  })
+  });
 
-  if (!subaccountDetails) return
-  if (!subaccountDetails.connectAccountId) return
+  if (!subaccountDetails) return;
+  if (!subaccountDetails.connectAccountId) return;
   const products = await getConnectAccountProducts(
     subaccountDetails.connectAccountId
-  )
+  );
 
   return (
     <div className="flex gap-4 flex-col xl:!flex-row">
@@ -56,18 +55,15 @@ const FunnelSettings: React.FC<FunnelSettingsProps> = async ({
                 products={products}
               />
             ) : (
-              'Connect your stripe account to sell products.'
+              "Connect your stripe account to sell products."
             )}
           </>
         </CardContent>
       </Card>
 
-      <FunnelForm
-        subAccountId={subaccountId}
-        defaultData={defaultData}
-      />
+      <FunnelForm subAccountId={subaccountId} defaultData={defaultData} />
     </div>
-  )
-}
+  );
+};
 
-export default FunnelSettings
+export default FunnelSettings;
